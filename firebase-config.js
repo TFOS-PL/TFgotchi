86623e1a2ce4a35aceecfd3954bb9180460b8830
TFOS-PL <homelklk7@gmail.com>
2025-01-13 20:06:53 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyBeYY5ZYJiE8nkKqr874ZpMJo1BTtq3YZM",
+  authDomain: "tfgotchi.firebaseapp.com",
+  projectId: "tfgotchi",
+  storageBucket: "tfgotchi.appspot.com",
+  messagingSenderId: "835432240305",
+  appId: "1:835432240305:web:9f01587a9749a45053be90",
+  measurementId: "G-XB1VWX1K55"
+};
+firebase.initializeApp(firebaseConfig);
